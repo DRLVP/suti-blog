ac35d167fbafdf362cac92a796f4fdcd41b29f29
drlov <phukondurlov45@gmail.com>
2024-03-10 23:59:24 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="w-screen h-screen bg-blue-950">
+      <h1 className="text-center text-4xl text-white">SUTI BLOG</h1>
+    </div>
+  )
+}
+
+export default App
