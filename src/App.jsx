955529ee00b7eae4b52514b7730ef5ac13921671
drlov <phukondurlov45@gmail.com>
2024-03-11 08:55:31 -0400

    complete appwrite authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
-
 function App() {
-
-  console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <div className="w-screen h-screen bg-blue-950">
       <h1 className="text-center text-4xl text-white">SUTI BLOG</h1>
+
+      
     </div>
   )
 }
---
+++ src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf"
+
+export class AuthService {
+    client =  new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        
+        this.account = new Account(this.client)
+    }
+
+    // make create account method
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+
+            if (userAccount) {
+                // call another method
+                return this.signIn(email, password)
+            }else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // make signin method
+    async signIn({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // make getCurrentUser Method
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+
+        return null;
+    }
+
+    // make logout method
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
