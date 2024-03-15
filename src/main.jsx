9df937f67a26d5e275360a6cafd169be4889a8e0
drlov <phukondurlov45@gmail.com>
2024-03-14 23:33:50 -0400

    complete routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,77 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from "./store/store.js"
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {Home, LoginPage, SignupPage, AddPostPage, AllPostPage, EditPostPage, PostPage} from "./pages/index.js"
+import { AuthLayout, Login } from './components/index.js'
+
+
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element:<App/>,
+      children:[
+         {
+            path:"/",
+            element:<Home/>
+         },
+         {
+            path:"/login",
+            element:(
+               <AuthLayout authentication={false}>
+                  <Login/>
+               </AuthLayout>
+            )
+         },
+         {
+            path:"/signup",
+            element:(
+               <AuthLayout authentication={false}>
+                  <SignupPage/>
+               </AuthLayout>
+            )
+         },
+         {
+            path:"/all-posts",
+            element:(
+               <AuthLayout authentication>
+                  {" "}
+                  <AllPostPage/>
+               </AuthLayout>
+            )
+         },
+         {
+            path:"/add-post",
+            element:(
+               <AuthLayout authentication>
+                  {" "}
+                  <AddPostPage/>
+               </AuthLayout>
+            )
+         },
+         {
+            path:"/edit-post/:slug",
+            element:(
+               <AuthLayout authentication>
+                  {" "}
+                  <EditPostPage/>
+               </AuthLayout>
+            )
+         },
+         {
+            path:"/post/:slug",
+            element:<PostPage/>
+         }
+      ]
+   }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
   <Provider store={store}>
      <React.StrictMode>
-        <App />
+        <RouterProvider router={router}/>
      </React.StrictMode>
   </Provider>,
 )
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import LoginPage from "./Login";
+import SignupPage from "./Signup";
+import PostPage from "./Post";
+import AddPostPage from "./AddPost";
+import AllPostPage from "./AllPost";
+import EditPostPage from "./EditPost";
+
+export {
+    Home,
+    LoginPage,
+    SignupPage,
+    PostPage,
+    AddPostPage,
+    AllPostPage,
+    EditPostPage
+}
