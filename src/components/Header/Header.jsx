7b1b739b5d3548c190764be5fafe7aaa000029d5
drlov <phukondurlov45@gmail.com>
2024-04-01 14:06:37 +0530

    create a hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,18 @@ import {Container, Logo, LogoutBtn} from "../../components/index"
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { MenuButton, X, XCircle } from 'react-bootstrap-icons'
 
 
 const Header = () => {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate()
 
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
   const navItems = [
     {
       name:"Home",
@@ -37,36 +43,76 @@ const Header = () => {
     }
   ]
   return (
-    <header className="w-screen h-24 shadow-xl bg-[#050404]">
+    <header className="w-full bg-[#050404]">
       <Container>
-        <nav className="w-full h-full flex justify-between px-8 items-center">
+        <nav className="w-full flex justify-between items-center">
           <div>
-            <Link to="/">
-              <Logo width="40px"/>
+            <Link>
+              <Logo/>
             </Link>
           </div>
-          <ul className="flex gap-4">
+          <ul className="flex-1 flex justify-end items-center gap-8 max-lg:hidden">
             {
-              navItems.map(item => 
+              navItems.map((item)=>(
                 item.active?
-                  <div key={item.name}>
-                    <li className="text-[1.3rem] text-[#DADADA] hover:text-[#951ABE] transition-colors">
-                      <button onClick={()=> navigate(item.url)}>{item.name}</button>
-                    </li>
-                  </div>
-                :null)
+                <li key={item.name} className="text-lg font-semibold text-[#DADADA] hover:text-[#951ABE] transition-all active:text-[#951ABE]">
+                  <Link to={item.url}>
+                    {item.name}
+                  </Link>
+                </li>:null
+              ))
             }
-            {
-              authStatus && (
-                <li>
-                  <LogoutBtn/>
-                </li>
-              )
-            }
+            <div>
+              {
+                authStatus && <LogoutBtn/>
+              }
+            </div>
           </ul>
+          <div
+            className="hidden max-lg:block cursor-pointer"
+            onClick={() => {
+              setIsMenuOpen(!isMenuOpen)
+            }}
+          >
+            <MenuButton className='text-[#DADADA] text-2xl font-semibold'/>
+          </div>
+          {isMenuOpen && (
+            <div>
+              <nav className="fixed top-0 right-0  bottom-0 lg:bottom-auto bg-[#DADADA] w-1/2 z-50">
+                <div
+                  className="hidden max-lg:block fixed right-[-0px]  px-8 py-4 cursor-pointer"
+                  onClick={() => {
+                    setIsMenuOpen(!isMenuOpen);
+                  }}
+                >
+                  <X className="text-4xl text-[#011A25]" />
+                </div>
+                <ul className=" lg:hidden flex flex-col items-center justify-center h-full ">
+                  {navItems.map((item) => (
+                    item.active?
+                    <li key={item.name} className='mb-4'>
+                      <Link
+                        to={item.url}
+                        className="font-semibold text-lg text-[#011A25]"
+                      >
+                        {item.name}
+                      </Link>
+                    </li>:null
+                  ))}
+
+                <div>
+                  {
+                    authStatus && <LogoutBtn/>
+                  }
+                </div>
+                </ul>
+              </nav>
+            </div>
+          )}
         </nav>
       </Container>
     </header>
+   
   )
 }
 
