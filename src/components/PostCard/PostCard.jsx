94f8ef1d30e624883d03875ef7f4bcf073e3311f
drlov <phukondurlov45@gmail.com>
2024-04-04 12:09:59 +0530

    fix images

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -9,13 +9,13 @@ const PostCard = ({$id, title, featuredImage, content}) => {
         <div className="card w-80 bg-base-100 shadow-xl">
             <Link to={`/post/${$id}`}>
                 <figure>
-                    <img src={service.getFilePreview(featuredImage)} alt={title} className="rounded-t-md object-cover"/>
+                    <img src={service.getFilePreview(featuredImage)} alt={title} className="h-[200px] w-full rounded-t-md object-cover"/>
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">
                         {title} <ArrowUpRight className="h-4 w-4" />
                     </h2>
-                    <p className="text-ellipsis overflow-hidden line-clamp-3">{parse(`${content}...`)}</p>
+                    <p className="text-ellipsis overflow-hidden line-clamp-2">{parse(`${content}...`)}</p>
                 </div>
             </Link>
         </div>
